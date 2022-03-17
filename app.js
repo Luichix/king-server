c2feeda2bbb4ba1e467660e6c251b2ce086f1567
Luichix <luigui.rex@gmail.com>
2022-03-17 15:21:39 -0600

    agregando configuracion

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-// const config = require('./utils/config')
+const config = require('./utils/config')
 const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
-const logger = require('./utils/logger')
 
-logger.info('connecting to')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB', error.message)
+  })
 
 app.use(cors())
+
+app.use(middleware.requestLogger)
+
+app.use(middleware.unknownEndpoint)
 
 
 module.exports = app
