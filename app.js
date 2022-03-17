6c6ffba4804873bc12dbd9cb5330c59f929cd8ba
Luichix <luigui.rex@gmail.com>
2022-03-17 15:57:10 -0600

    agregando rutas y modelos

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
-
+const usersRouter = require('./controllers/user')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -19,10 +20,16 @@ mongoose.connect(config.MONGODB_URI)
   })
 
 app.use(cors())
-
+app.use(express.static('build'))
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(middleware.requestLogger)
 
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
 app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 
 module.exports = app
